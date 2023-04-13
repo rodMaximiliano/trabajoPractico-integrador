13c33607ff4f950762a48465396af964ff4ba23d
rodMaximiliano <maximilianorodrigez.mr@gmail.com>
2023-04-13 20:37:50 -0300

    Avances en Personal, falta poco

--- js/cotizacionPasos.js
+++ js/cotizacionPasos.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     // Acceder del paso 1 al paso 2
     $("#sigPaso1").on("click", function() {
         let nombrePersona = $("#name").val();
@@ -17,6 +18,12 @@ $(document).ready(function() {
     });
     // Acceder del paso 2 al paso 3
     $(`#sigPaso2`).on(`click`, function() {
+        let nombreEmpresa = $("#nameEmpresa").val();
+        let apellidoEmpresa = $("#lastName").val();
+        let emailEmpresa = $("email").val();
+        console.log(nombreEmpresa);
+        console.log(apellidoEmpresa);
+        console.log(emailEmpresa);
         $(`#paso2`).hide();
         $(`#paso3`).show();
     });
@@ -25,4 +32,5 @@ $(document).ready(function() {
         $(`#paso3`).hide();
         $(`#paso2`).show();
     });
+
 });
